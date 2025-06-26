17574435e24bcb3c1e4a87e39bb431548b4da94c
Suci Ihtisabi Hida Nursyifa <118999021+Ihtisabi@users.noreply.github.com>
2025-06-26 22:10:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,6 @@ def extract():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # pakai PORT dari Railway
+    app.run(host="0.0.0.0", port=port)
